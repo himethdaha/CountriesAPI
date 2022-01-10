645fb12f2117ae7223a30c5da6f496a9efb8f416
Himeth Dahanayake <himethdaha@gmail.com>
2022-01-10 17:20:19 -0500

    Using promises for the first time

--- script.js
+++ script.js
@@ -19,32 +19,32 @@ checkCountry.addEventListener("click", function () {
 
   //Function to display the main country
   const renderCountry = function (data) {
+    //Destructing the data obj
+    const [destdata] = data;
     //Values from the request
-    const languages = Object.values(data.languages);
-    const currencies = Object.values(data.currencies);
+    const languages = Object.values(destdata.languages);
+    const currencies = Object.values(destdata.currencies).map(
+      (curr) => curr.name
+    );
 
-    //Adding the html for the country
+    //     //Adding the html for the country
     const html = `
               <div class="country">
               <div class="flag-div">
                   <img
-                  src="${data.flags.svg}"
+                  src="${destdata.flags.svg}"
                   alt=""
                   class="flag"
                   />
               </div>
               <div class="country-info">
-                  <span class="country-name">${data.name.common}</span>
-                  <span class="continent">${data.continents}</span>
+                  <span class="country-name">${destdata.name.common}</span>
+                  <span class="continent">${destdata.continents}</span>
                   <p><span class="emoji">🧑🏼‍🤝‍🧑🏽</span><span class="population fax">${(
-                    data.population / 1000000
+                    destdata.population / 1000000
                   ).toFixed(2)}M</span></p>
-                  <p><span class="emoji">🗣️</span><span class="language fax">${
-                    languages[0]
-                  }</span></p>
-                  <p><span class="emoji">💰</span><span class="currency fax">${
-                    currencies[0].name
-                  }</span></p>
+                  <p><span class="emoji">🗣️</span><span class="language fax">${languages}</span></p>
+                  <p><span class="emoji">💰</span><span class="currency fax">${currencies}</span></p>
               </div>
               </div>`;
 
@@ -52,75 +52,97 @@ checkCountry.addEventListener("click", function () {
     containerForCountries.insertAdjacentHTML("beforeend", html);
   };
 
-  //Function to display the neighboring country
-  const renderNeighbor = function (data) {
-    //Values from the request
-    const languages = Object.values(data.languages);
-    const currencies = Object.values(data.currencies);
-
-    //Adding the html for the country
-    const html = `
-              <div class="country-neighbor">
-              <div class="flag-div-neighbor">
-                  <img
-                  src="${data.flags.svg}"
-                  alt=""
-                  class="flag"
-                  />
-              </div>
-              <div class="country-info-neighbor">
-                  <span class="country-name-neighbor">${data.name.common}</span>
-                  <span class="continent">${data.continents}</span>
-                  <p><span class="emoji">🧑🏼‍🤝‍🧑🏽</span><span class="population fax">${(
-                    data.population / 1000000
-                  ).toFixed(2)}M</span></p>
-                  <p><span class="emoji">🗣️</span><span class="language fax">${
-                    languages[0]
-                  }</span></p>
-                  <p><span class="emoji">💰</span><span class="currency fax">${
-                    currencies[0].name
-                  }</span></p>
-              </div>
-              </div>`;
-
-    //Insert html into the container
-    containerForCountries.insertAdjacentHTML("beforeend", html);
-  };
+  ////////////////// USING PROMISES///////////////
+  //using fetch api
+  //Using a promise to get the country
   const getCountry = function (country) {
-    //Creating the http request
-    const request = new XMLHttpRequest();
-    //Opening the request to an API
-    request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-    //Sending the request to the API
-    request.send();
+    //This returns a promise
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+      .then(function (response) {
+        console.log(response);
+        //To read the response use JSON. which also returns a promise
+        return response.json();
+      })
+      //Since the json methodreturns a promise I can use 'then' on the entire callback function
 
-    //Adding an eventlistener to the 'load' event from the request
-    request.addEventListener("load", function () {
-      //this -> request
-      //Getting the response as a JS object
-      const [data] = JSON.parse(this.responseText);
-      //Get the neighbor array
-      const [neighbor] = data.borders;
-      //If there aren't any neighbors just return
-      if (!neighbor) return;
-      //For the main country
-      renderCountry(data);
-
-      //AJAX call for the neighboring country
-      const request2 = new XMLHttpRequest();
-      //Opening the request to an API
-      //Since neighbor returns a code, use the code in the URL
-      request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbor}`);
-      //Sending the request to the API
-      request2.send();
-
-      //Adding an eventlistener to the neighboring country
-      request2.addEventListener("load", function () {
-        //get the data
-        const [data2] = JSON.parse(this.responseText);
-        renderNeighbor(data2);
+      .then(function (data) {
+        console.log(data);
+        renderCountry(data);
       });
-    });
   };
+
   getCountry(countryValue);
 });
+
+//   //Function to display the neighboring country
+//   const renderNeighbor = function (data) {
+//     //Values from the request
+//     const languages = Object.values(data.languages);
+//     const currencies = Object.values(data.currencies);
+
+//     //Adding the html for the country
+//     const html = `
+//               <div class="country-neighbor">
+//               <div class="flag-div-neighbor">
+//                   <img
+//                   src="${data.flags.svg}"
+//                   alt=""
+//                   class="flag"
+//                   />
+//               </div>
+//               <div class="country-info-neighbor">
+//                   <span class="country-name-neighbor">${data.name.common}</span>
+//                   <span class="continent">${data.continents}</span>
+//                   <p><span class="emoji">🧑🏼‍🤝‍🧑🏽</span><span class="population fax">${(
+//                     data.population / 1000000
+//                   ).toFixed(2)}M</span></p>
+//                   <p><span class="emoji">🗣️</span><span class="language fax">${
+//                     languages[0]
+//                   }</span></p>
+//                   <p><span class="emoji">💰</span><span class="currency fax">${
+//                     currencies[0].name
+//                   }</span></p>
+//               </div>
+//               </div>`;
+
+//     //Insert html into the container
+//     containerForCountries.insertAdjacentHTML("beforeend", html);
+//   };
+//   const getCountry = function (country) {
+//     //Creating the http request
+//     const request = new XMLHttpRequest();
+//     //Opening the request to an API
+//     request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+//     //Sending the request to the API
+//     request.send();
+
+//     //Adding an eventlistener to the 'load' event from the request
+//     request.addEventListener("load", function () {
+//       //this -> request
+//       //Getting the response as a JS object
+//       const [data] = JSON.parse(this.responseText);
+//       console.log(data);
+//       //For the main country
+//       renderCountry(data);
+
+//       //Get the neighbor array and destruct it to get the code
+//       const [neighbor] = data.borders;
+//       if (!neighbor) return;
+//       //AJAX call for the neighboring country
+//       const request2 = new XMLHttpRequest();
+//       //Opening the request to an API
+//       //Since neighbor returns a code, use the code in the URL
+//       request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbor}`);
+//       //Sending the request to the API
+//       request2.send();
+
+//       //Adding an eventlistener to the neighboring country
+//       request2.addEventListener("load", function () {
+//         //get the data
+//         const [data2] = JSON.parse(this.responseText);
+//         renderNeighbor(data2);
+//       });
+//     });
+//   };
+//   getCountry(countryValue);
+//
