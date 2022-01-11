ae8d6a34b43f643421bfe0dd4debb52fa78271f8
Himeth Dahanayake <himethdaha@gmail.com>
2022-01-11 16:25:30 -0500

    retrieve all neighboring countries through promises

--- script.js
+++ script.js
@@ -12,9 +12,14 @@ const alertclose = document.querySelector(".close-btn");
 const searchbyName = document.querySelector(".name-search");
 const searchbyCoords = document.querySelector(".coords-search");
 const checkCountryForm = document.querySelector(".country-form");
-const getCoordsForm = document.querySelector(".coordinates-form");
+const getCoordsForm = document.querySelector(".coords-search");
+const getCoords = document.querySelector(".coordinates-form");
 const drop = document.querySelector(".drop-btn");
 const mydrop = document.querySelector(".my-dropdown");
+let lng;
+let lat;
+let link = [];
+let countries = [];
 
 //Show all the options when clicking the dropdown
 drop.addEventListener("click", function () {
@@ -35,8 +40,6 @@ getCoordsForm.addEventListener("click", function () {
   mydrop.style.display = "none";
 });
 
-//On coords form submit
-
 //On form submit
 checkCountrySubmit.addEventListener("click", function () {
   //Get the value inside the input form
@@ -44,6 +47,16 @@ checkCountrySubmit.addEventListener("click", function () {
 
   getCountry(countryValue);
 });
+
+//On coords form submit
+coordsSubmit.addEventListener("click", function () {
+  //Get the coords on form submit
+  lat = document.getElementById("latitude-txts").value;
+  lng = document.getElementById("longitude-txts").value;
+
+  getCountrybyCoords(lat, lng);
+});
+
 //Function to display the main country
 const renderCountry = function (data) {
   //Destructing the data obj
@@ -84,8 +97,9 @@ const renderCountry = function (data) {
 
 //   //Function to display the neighboring country
 const renderNeighbor = function (data) {
-  //Destructing the data obj
+  console.log(data); //Destructing the data obj
   const [destdata] = data;
+
   //Values from the request
   const languages = Object.values(destdata.languages);
   const currencies = Object.values(destdata.currencies).map(
@@ -122,6 +136,135 @@ const renderNeighbor = function (data) {
   containerForCountries.insertAdjacentHTML("beforeend", html);
 };
 
+//Function to display the main country
+const renderCountrybyCoords = function (data) {
+  //Destructing the data obj
+  const [destdata] = data;
+  console.log(destdata);
+  //Values from the request
+  const languages = Object.values(destdata.languages);
+  const currencies = Object.values(destdata.currencies).map(
+    (curr) => curr.name
+  );
+
+  //     //Adding the html for the country
+  const html = `
+                <div class="country">
+                <div class="flag-div">
+                    <img
+                    src="${destdata.flags.svg}"
+                    alt=""
+                    class="flag"
+                    />
+                </div>
+                <div class="country-info">
+                    <span class="country-name">${destdata.name.common}</span>
+                    <span class="continent">${destdata.continents}</span>
+                    <p><span class="emoji">🧑🏼‍🤝‍🧑🏽</span><span class="population fax">${(
+                      destdata.population / 1000000
+                    ).toFixed(2)}M</span></p>
+                    <p><span class="emoji">🚩</span><span class="capital fax">${
+                      destdata.capital
+                    }</span></p>
+                    <p><span class="emoji">🗣️</span><span class="language fax">${languages}</span></p>
+                    <p><span class="emoji">💰</span><span class="currency fax">${currencies}</span></p>
+                </div>
+                </div>`;
+
+  //Insert html into the container
+  containerForCountries.insertAdjacentHTML("beforeend", html);
+};
+
+//For the coords
+const getCountrybyCoords = function (lat, lng) {
+  //using fetch api
+  //Using a promise to to get the country
+  fetch(
+    `https://geocode.xyz/${lat},${lng}?geoit=json&auth=894954341150912812594x2174`
+  )
+    .then(function (response) {
+      //Validating the response returned
+      //check if the field is blank or is a string
+      if (lat.length === 0 || lng.length === 0 || isNaN(lat) || isNaN(lng)) {
+        throw new Error("Enter two numeric values please!!! 🙃");
+      }
+
+      //If status message isn't ok
+      if (!response.ok) {
+        throw new Error(`Country not found ${response.status}`);
+      }
+      //return the response
+      return response.json();
+    })
+    //data is the response returned by the before then method
+    .then(function (data) {
+      console.log(data);
+      //destruct the array of objs
+      const newdata = [data];
+      console.log(newdata);
+      //Get the country name
+      const countryname = newdata[0].country;
+      console.log(countryname);
+
+      //Return the countryname into a fetch url to use the restcountries API
+      return fetch(`https://restcountries.com/v3.1/name/${countryname}`);
+    })
+    //Get the response from the before then method
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    //Display  the country info
+    .then(function (data) {
+      renderCountry(data);
+
+      //Time to get the neighboring country
+      //Check for borders
+      if (!("borders" in data[0] === true)) {
+        throw new Error("No neighboring countries but that's okay 🙂");
+      }
+      //Save the neighbors in an array
+      const neighbors = data[0].borders;
+      console.log(neighbors);
+      //Fetch all the neighboring country links
+      for (let i = 0; i < neighbors.length; i++) {
+        countries.push(
+          fetch(`https://restcountries.com/v3.1/alpha/${[neighbors[i]]}`)
+        );
+      }
+      //Return the responses as a promise
+      return Promise.all(countries);
+    })
+    .then(function (response) {
+      console.log(response);
+      //Convert the entire promise into json
+      return Promise.all(response.map((link) => link.json()));
+    })
+    .then(function (data) {
+      console.log(data);
+      //Loop through the array getting individual arrays and give them to the renderNeighbor method
+      for (let i = 0; i < data.length; i++) {
+        console.log(data[i]);
+        renderNeighbor(data[i]);
+      }
+      //   renderCountrybyCoords(data);
+    })
+    // .then(function (response) {
+    //   console.log(response);
+    //   return response.json();
+    // })
+    // .then(function (data) {
+    //   console.log(data);
+    //})
+    //Making the container for the information visibile
+    .finally(function () {
+      containerForCountries.style.opacity = 1;
+    });
+  //Render the html for the neighboring country
+};
+//52.508
+//13.381
+
 ////////////////// USING PROMISES///////////////
 //using fetch api
 //Using a promise to get the country
@@ -150,16 +293,32 @@ const getCountry = function (country) {
       }
 
       //Get the neighboring country from the array of objs
-      const neighbor = data[0].borders[0];
+      const neighbor = data[0].borders;
+      console.log(neighbor);
       //Return the neighboring country
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbor}`);
+      //
+      for (let i = 0; i < neighbor.length; i++) {
+        countries.push(
+          fetch(`https://restcountries.com/v3.1/alpha/${[neighbor[i]]}`)
+        );
+      }
+      //Return the responses as a promise
+      return Promise.all(countries);
     })
     //This returns a promise
     .then(function (response) {
-      return response.json();
+      console.log(response);
+      //   return response.json();
+      //Convert the entire promise into json
+      return Promise.all(response.map((link) => link.json()));
     })
     .then(function (data) {
-      renderNeighbor(data);
+      console.log(data);
+      //   renderNeighbor(data);
+      for (let i = 0; i < data.length; i++) {
+        console.log(data[i]);
+        renderNeighbor(data[i]);
+      }
     })
     //Catch an error
     .catch(function (err) {
